6d1b51537642db156483afa997a3c1b77f1e96eb
J. Famous <joshua.famous@gmail.com>
2017-07-02 23:48:47 -0400

    Initial File Commit
    All the stuff I did to start the project

---
+++ scripts.js
@@ -0,0 +1,63 @@
+function sidebarToggle(){
+	//declare variables
+	var openorclose;	//specify the action to perform
+	var sidebarWidth;	//current width of the sidebar
+	var contentWidth;	//current width of the content (main and titlebar)
+
+	//check the action, and start that action
+	if(document.getElementById("sidebar").style.display == "none"){
+		openorclose = 0;
+		contentWidth = 100;
+		sidebarWidth = 0;
+		var intervalId = setInterval(sidebarOpen,10);
+  	}
+  	else{
+  		openorclose = 1;
+  		contentWidth = 75;
+		sidebarWidth = 25;
+  		var intervalId = setInterval(sidebarClose,10);
+  	}
+
+  	//perform visibility changes
+  	if(openorclose == 0){
+	  	document.getElementById("sidebar").style.display = "block";
+	  	document.getElementById("sidebarContent").style.display = "block";
+  	}
+
+  	//animation updates
+  	function sidebarOpen(){
+  		//update variables
+  		contentWidth -= 1;
+  		sidebarWidth += 1;
+  		//perform actions
+  		document.getElementById("main").style.width = contentWidth + '%';
+  		document.getElementById("titlebar").style.width = contentWidth + '%';
+  		document.getElementById("main").style.marginLeft = sidebarWidth + '%';
+  		document.getElementById("titlebar").style.marginLeft = sidebarWidth + '%';
+  		document.getElementById("sidebar").style.width = sidebarWidth + '%';
+  		//check animation end
+  		if(sidebarWidth == 25){
+  			clearInterval(intervalId);
+  		}
+  	}
+
+  	function sidebarClose(){
+  		//update variables
+  		contentWidth += 1;
+  		sidebarWidth -= 1;
+  		//perform actions
+  		document.getElementById("main").style.width = contentWidth + '%';
+  		document.getElementById("titlebar").style.width = contentWidth + '%';
+  		document.getElementById("main").style.marginLeft = sidebarWidth + '%';
+  		document.getElementById("titlebar").style.marginLeft = sidebarWidth + '%';
+  		document.getElementById("sidebar").style.width = sidebarWidth + '%';
+  		//check animation end
+  		if(sidebarWidth == 0){
+  			document.getElementById("sidebar").style.display = "none";
+  			clearInterval(intervalId);
+  		}
+  		if(sidebarWidth <= 15){
+  			document.getElementById("sidebarContent").style.display = "none";
+  		}
+  	}
+}
